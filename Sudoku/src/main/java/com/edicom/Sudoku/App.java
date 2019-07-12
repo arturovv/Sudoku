7143d39676cbad6872fb084937f188f15eb61f36
avaron <avaron@ED1939.edicom.es>
2019-07-12 13:44:44 +0200

    first commit

---
+++ Sudoku/src/main/java/com/edicom/Sudoku/App.java
@@ -0,0 +1,13 @@
+package com.edicom.Sudoku;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
